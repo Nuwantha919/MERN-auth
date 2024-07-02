51932bf44601cb76d59491bcb24cfdc0fb58be42
Nuwantha Somawansha <e19492@eng.pdn.ac.lk>
2024-07-02 13:58:13 +0530

    middleware

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+    // console.log("REQ BODY ON SIGNUP", req.body);
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10); //= await bcryptjs.hash(password, 10); to remove await hash--> hashSync
+    
+    const newUser = new User({ username, email, password: hashedPassword});
+    try {
+
+        await newUser.save()
+        res.status(201).json({ message: "user created successfully" });
+    
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+
+};
